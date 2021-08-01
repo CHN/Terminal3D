ad27e4f4b462b3dc3664e702737076168fb410fa
Cihan Sakarya <cihansakaryacs@gmail.com>
2021-08-01 18:23:21 +0300

    feat: Implement console manager for Windows

--- includes/Terminal3D/PlatformDependent/ConsoleManager.h
+++ includes/Terminal3D/PlatformDependent/ConsoleManager.h
@@ -1,7 +1,20 @@
 #pragma once
 
+#include "Terminal3D/Math.h"
+
 class ConsoleManager
 {
 public:
-	InitializeConsole();
+	static bool TryToInitializeConsole();
+	static bool TryToInitializeConsole(Vector2DI screenSize);
+
+	static void SleepForMilliseconds(size_t duration);
+
+	static void PrintScreenBuffer(const char* screenBuffer, size_t screenBufferSize);
+
+	static Vector2DI GetScreenBufferSize() { return screenBufferSize; }
+
+private:
+
+	static Vector2DI screenBufferSize;
 };
--- src/Terminal3D/PlatformDependent/Windows/ConsoleManager.cpp
+++ src/Terminal3D/PlatformDependent/Windows/ConsoleManager.cpp
@@ -1,1 +1,61 @@
 #include "Terminal3D/PlatformDependent/ConsoleManager.h"
+
+#define _WIN32_WINNT 0x0500
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+Vector2DI ConsoleManager::screenBufferSize;
+
+namespace
+{
+	void ShowConsoleCursor(bool showFlag)
+	{
+		HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+
+		CONSOLE_CURSOR_INFO     cursorInfo;
+
+		GetConsoleCursorInfo(out, &cursorInfo);
+		cursorInfo.bVisible = showFlag; // set the cursor visibility
+		SetConsoleCursorInfo(out, &cursorInfo);
+	}
+}
+
+bool ConsoleManager::TryToInitializeConsole()
+{
+	return TryToInitializeConsole({ 1280, 720 });
+}
+
+bool ConsoleManager::TryToInitializeConsole(Vector2DI screenSize)
+{
+	system("cls");
+
+	HWND console = GetConsoleWindow();
+	RECT r;
+	GetWindowRect(console, &r);
+
+	MoveWindow(console, r.left, r.top, screenSize.x, screenSize.y, TRUE);
+
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+
+	screenBufferSize.x = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+	screenBufferSize.y = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+
+	ShowConsoleCursor(false);
+
+	return true;
+}
+
+void ConsoleManager::SleepForMilliseconds(size_t duration)
+{
+	Sleep(static_cast<DWORD>(duration));
+}
+
+void ConsoleManager::PrintScreenBuffer(const char* screenBuffer, size_t screenBufferSize)
+{
+	HANDLE output = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleCursorPosition(output, { 0, 0 });
+
+	fwrite(screenBuffer, sizeof(char), screenBufferSize, stdout);
+}
