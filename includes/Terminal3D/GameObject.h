13361439fd3838457f852b0e3a1882f921bf3ae8
Cihan Sakarya <cihansakaryacs@gmail.com>
2021-08-19 23:02:23 +0300

    feat: Implement gameobject

---
+++ includes/Terminal3D/GameObject.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "Math.h"
+
+// Consider migrating to ECS
+
+class GameObject
+{
+public:
+
+	GameObject();
+
+	Vector3DF GetPosition() const { return m_Position; }
+	virtual void SetPosition(Vector3DF val) 
+	{ 
+		m_Position = val;
+		UpdateTransformationMatrixCache();
+	}
+
+	Vector3DF GetScale() const { return m_Scale; }
+	virtual void SetScale(Vector3DF val) 
+	{
+		m_Scale = val;
+		UpdateTransformationMatrixCache();
+	}
+
+	QuaternionF GetRotation() const { return m_Rotation; }
+	virtual void SetRotation(QuaternionF val) 
+	{ 
+		m_Rotation = val;
+		UpdateTransformationMatrixCache(); 
+	}
+
+	const MatrixF4x4& GetTransformationMatrix() const { return m_TransformationMatrixCache; }
+
+	virtual ~GameObject() = default;
+
+protected:
+
+	Vector3DF m_Position;
+	Vector3DF m_Scale;
+	QuaternionF m_Rotation;
+
+private:
+	MatrixF4x4 m_TransformationMatrixCache;
+
+	void UpdateTransformationMatrixCache(); // Consider using dirtying method
+};
---
+++ src/Terminal3D/GameObject.cpp
@@ -0,0 +1,54 @@
+#include "Terminal3D/GameObject.h"
+
+GameObject::GameObject()
+	: m_Position(Vector3DF::Zero), m_Scale(1,1,1)
+{
+
+}
+
+void GameObject::UpdateTransformationMatrixCache()
+{
+	auto p = m_Position * -1;
+	auto& s = m_Scale;
+	auto q = m_Rotation;
+	q.Inverse();
+
+	MatrixF4x4 sm;
+
+	sm.m[0][0] = s.x; sm.m[0][1] = 0;   sm.m[0][2] = 0;   sm.m[0][3] = 0;
+	sm.m[1][0] = 0;   sm.m[1][1] = s.y; sm.m[1][2] = 0;   sm.m[1][3] = 0;
+	sm.m[2][0] = 0;   sm.m[2][1] = 0;   sm.m[2][2] = s.z; sm.m[2][3] = 0;
+	sm.m[3][0] = 0;   sm.m[3][1] = 0;   sm.m[3][2] = 0;   sm.m[3][3] = 1;
+
+	MatrixF4x4 rm;
+
+	rm.m[0][0] = 1.f - 2.f * q.y * q.y - 2.f * q.z * q.z;
+	rm.m[0][1] = 2.f * q.x * q.y - 2.f * q.z * q.w;
+	rm.m[0][2] = 2.f * q.x * q.z + 2.f * q.y * q.w;
+	rm.m[0][3] = 0;
+
+	rm.m[1][0] = 2.f * q.x * q.y + 2.f * q.z * q.w;
+	rm.m[1][1] = 1.f - 2.f * q.x * q.x - 2.f * q.z * q.z;
+	rm.m[1][2] = 2.f * q.y * q.z - 2.f * q.x * q.w;
+	rm.m[1][3] = 0;
+
+	rm.m[2][0] = 2.f * q.x * q.z - 2.f * q.y * q.w;
+	rm.m[2][1] = 2.f * q.y * q.z + 2.f * q.x * q.w;
+	rm.m[2][2] = 1.f - 2.f * q.x * q.x - 2.f * q.y * q.y;
+	rm.m[2][3] = 0;
+
+	rm.m[3][0] = 0;
+	rm.m[3][1] = 0;
+	rm.m[3][2] = 0;
+	rm.m[3][3] = 1;
+
+	MatrixF4x4 pm;
+
+	pm.m[0][0] = 1;   pm.m[0][1] = 0;   pm.m[0][2] = 0;   pm.m[0][3] = p.x;
+	pm.m[1][0] = 0;   pm.m[1][1] = 1;   pm.m[1][2] = 0;   pm.m[1][3] = p.y;
+	pm.m[2][0] = 0;   pm.m[2][1] = 0;   pm.m[2][2] = 1;   pm.m[2][3] = p.z;
+	pm.m[3][0] = 0;   pm.m[3][1] = 0;   pm.m[3][2] = 0;   pm.m[3][3] = 1;
+
+	m_TransformationMatrixCache = pm * rm * sm;
+}
+
